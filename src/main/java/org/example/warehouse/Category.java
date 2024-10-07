71ee909fe6d6577f958f94b454d9961acd94f11e
Jenny Magnusson <jenny.km.magnusson@gmail.com>
2024-10-07 16:06:14 +0200

    Warehouse, Category and ProductRecord are done

--- src/main/java/org/example/warehouse/Category.java
+++ src/main/java/org/example/warehouse/Category.java
@@ -1,5 +1,7 @@
 package org.example.warehouse;
 
+import java.util.Objects;
+
 public class Category {
 
     private String name;
@@ -17,5 +19,17 @@ public class Category {
             throw new IllegalArgumentException("Category name can't be null");
         }
         return new Category(name);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Category category)) return false;
+        return Objects.equals(name, category.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
     }
 }
--- src/main/java/org/example/warehouse/Warehouse.java
+++ src/main/java/org/example/warehouse/Warehouse.java
@@ -3,6 +3,7 @@ package org.example.warehouse;
 
 import java.math.BigDecimal;
 import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Warehouse {
@@ -11,7 +12,6 @@ public class Warehouse {
     private static Warehouse instance;
     private List<ProductRecord> products = new ArrayList<>();
     private List<ProductRecord> changedProducts = new ArrayList<>();
-
 
     private Warehouse(String name) {
         this.name = name;
@@ -26,7 +26,6 @@ public class Warehouse {
         if(instance == null) {
             instance = new Warehouse(name);
         }
-
         return instance;
     }
 
@@ -41,75 +40,104 @@ public class Warehouse {
         return List.copyOf(products);
     }
 
-    // todo: find multiple products from same category
     public ProductRecord addProduct(UUID uuid, String name, Category category, BigDecimal price)   {
         for(ProductRecord product : products) {
             if(product.uuid().equals(uuid)) {
                 throw new IllegalArgumentException("Product with that id already exists, use updateProduct for updates.");
             }
         }
-
         if(uuid == null ) {
             uuid = UUID.randomUUID();
         }
-
         if ( name == null || name.isEmpty() ) {
             throw new IllegalArgumentException("Product name can't be null or empty.");
         }
-
         if(category == null) {
             throw new IllegalArgumentException("Category can't be null.");
         }
-
         if(price == null) {
             price = BigDecimal.ZERO;
         }
-
         ProductRecord newProduct = new ProductRecord(uuid, category, price, name);
         products.add(newProduct);
 
         return newProduct;
     }
 
-    // todo: find changed products is empty
     public List<ProductRecord> getChangedProducts() {
-//        if(changedProducts.isEmpty()) {
-//            throw new IllegalArgumentException("Product list is empty");
-//        }
-        return Collections.singletonList(changedProducts.getFirst());
-    }
-
-    // todo: find changed products returns product
-    // todo: throws IllegalArgumentException when trying to change an invalid id
-    // todo: changing a products price should be saved
-    public void updateProductPrice(UUID uuid, BigDecimal bigDecimal) {
-
-    }
-
-    // todo: group them by category
-    public List<ProductRecord> getProductsGroupedByCategories() {
-
+        if(changedProducts.isEmpty()) {
+                return Collections.emptyList();
+            }
         return List.copyOf(changedProducts);
     }
 
-    // todo: find all products belonging to a category
-    public List<ProductRecord> getProductsBy(Category category)  {
-        return List.copyOf(products);
+    public void updateProductPrice(UUID uuid, BigDecimal price) {
+        boolean productFound = false;
+        ProductRecord oldProduct = null;
 
+        for(ProductRecord product : products) {
+            if(uuid.equals(product.uuid())) {
+                if(!product.price().equals(price)) {
+                    oldProduct = product;
+                    ProductRecord updatedProduct = new ProductRecord(product.uuid(), product.category(), price, product.productName());
+
+                    products.remove(product);
+                    products.add(updatedProduct);
+
+                    changedProducts.removeIf(p -> p.uuid().equals(uuid));
+                    changedProducts.add(oldProduct);
+                }
+                productFound = true;
+                break;
+            }
+       }
+
+        if(!productFound) {
+            throw new IllegalArgumentException("Product with that id doesn't exist.");
+        }
+    }
+
+
+    public Map<Category, List<ProductRecord>> getProductsGroupedByCategories() {
+        return products.stream()
+                .collect(Collectors.groupingBy(ProductRecord::category, Collectors.collectingAndThen(Collectors.toList(), Collections::unmodifiableList)));
+
+//        Map<Category, List<ProductRecord>> groupedProducts = new HashMap<>();
+//        for (ProductRecord product : products) {
+//            Category category = product.category();
+//
+//            groupedProducts.computeIfAbsent(category, k -> new ArrayList<>()).add(product);
+//        }
+//
+//        groupedProducts.replaceAll(((category, productList) -> List.copyOf(productList)));
+//
+//        return Collections.unmodifiableMap(groupedProducts);
+    }
+
+    public List<ProductRecord> getProductsBy(Category category)  {
+        return products.stream()
+                .filter(product -> product.category().equals(category))
+                .toList();
+
+//        return List.copyOf(products.stream()
+//                .filter(product -> product.category().equals(category))
+//                .toList());
     }
 
     public Optional<ProductRecord> getProductById(UUID uuid) {
-        if(products.isEmpty())
-            throw new RuntimeException("Product list is empty");
-
-        for(ProductRecord product : products) {
-            if(product.uuid().equals(uuid)) {
-                return Optional.of(product);
-            }
-        }
-        return Optional.empty();
+//        if(products.isEmpty())
+//            throw new RuntimeException("Product list is empty");
+//
+//        for(ProductRecord product : products) {
+//            if(product.uuid().equals(uuid)) {
+//                return Optional.of(product);
+//            }
+//        }
+//        return Optional.empty();
+        return products.stream()
+                .filter(product -> product.uuid().equals(uuid))
+                .findFirst();
     }
-
 }
 
 
